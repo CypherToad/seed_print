d4237d788ba4598f1631efce10228c4a9462c013
CypherToad <ribbit [at] void>
2021-12-13 21:31:46 -0600

    initial

---
+++ README.md
@@ -0,0 +1,52 @@
+# seed_print
+
+> **WARNING** This project has mainly been used for local development, use at your own risk, and always use on an air-gapped machine!
+
+A minimal python script for generatin **bip39 seed phrases**, and corresponding [Seed Signer Seed](https://github.com/SeedSigner/seedsigner) seed phrase **qr code**.
+
+![screenshot](/examples/screenshoot_v1.png)
+
+### Dependencies
+
+The following python project are used, and should be retrieved
+before moving to the final air-gapped machine.
+
+```
+certifi==2021.10.8
+charset-normalizer==2.0.9
+click==8.0.3
+Flask==2.0.2
+idna==3.3
+itsdangerous==2.0.1
+Jinja2==3.0.3
+MarkupSafe==2.0.1
+mnemonic==0.20
+Pillow==8.4.0
+qrcode==7.3.1
+requests==2.26.0
+urllib3==1.26.7
+Werkzeug==2.0.2
+```
+
+### Installation
+
+```
+$ cd seed_print/
+$ virtualenv -p python3 venv/
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+### Usage
+
+```
+$ ./main.py
+```
+
+If successful you will have a local website available with your
+random seed phrases.
+
+
+> * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+Go ahead and refresh the page to get a new set of addresses!
---
+++ main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+# Built in python libraries
+import json
+import base64
+from io import BytesIO
+from hashlib import sha256
+
+# Trusting Trezor's for randopm wallet creations
+from mnemonic import Mnemonic
+
+# Thanks @KeithMukai for the suggestion!
+# https://twitter.com/KeithMukai/status/1470571942000443392
+# Confirm we are offline
+import requests
+
+# Trusting qrcode library as offline qr code creation
+import qrcode
+
+# Trusting Flask as simple web interface which can be easily printed or stored as pdf
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+mnemo = Mnemonic("english")
+
+
+# load our words.txt which was fetched from Trezor's github
+# https://github.com/trezor/python-mnemonic/blob/master/src/mnemonic/wordlist/english.txt
+with open('words.txt') as f:
+    wordlist = [ i for i in f.read().split('\n') if i ]
+
+# confirm we have our 2048 words
+if len(wordlist) != 2048:
+    raise Exception("Word list is not correct length: %s" % len(wordlist))
+
+# confirm our checksum matches
+checksum = sha256(json.dumps(wordlist).encode()).hexdigest()
+if checksum != '9944a25d756463cef4038bd1b5e312932ec874f0236be654a977fa7cc49fb03a':
+    raise Exception("Checksum mistmatch!")
+
+
+def seed_qr_string(words):
+    """
+    Return a string representation of our words for seed_signer
+    """
+
+    return ''.join([str(wordlist.index(w)).zfill(4) for w in words.split()])
+
+
+def seed_qr_base64(words):
+    """
+    Return a base64 PNG encoding of our seed_qr.
+    """
+
+    # create a qrcode of our seed_qr_string
+    img = qrcode.make(
+        seed_qr_string(words))
+
+    # generate a
+    # https://jdhao.github.io/2020/03/17/base64_opencv_pil_image_conversion/
+    im_file = BytesIO()
+    img.save(im_file, format="PNG")
+    im_b64 = base64.b64encode(im_file.getvalue())
+
+    return im_b64.decode()
+
+
+@app.route("/")
+def home():
+    """
+    Main home page which generates random wallets
+    """
+
+    # generate a strong wallet
+    words = mnemo.generate(strength=256)
+    qr_string = seed_qr_string(words)
+
+    # Thanks for the suggestion @KeithMukai
+    # https://twitter.com/KeithMukai/status/1470571942000443392
+    # if requests.get('http://bitcoin.org'):
+    #     return render_template('panic.html')
+
+    return render_template(
+        'index.html', words=words, seed_qr=seed_qr_base64(words),
+        qr_string=qr_string)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
